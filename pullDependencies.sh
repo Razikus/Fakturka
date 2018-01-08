664948ddb448b22dd81521c542bb85279c83e2b1
Adam Raźniewski <adam.razniewski@gmail.com>
2018-01-08 17:39:18 +0100

    dependencies

---
+++ pullDependencies.sh
@@ -0,0 +1,14 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LIBS="$DIR/libs"
+smtpClient="$LIBS/smtpclient"
+mkdir -p $LIBS
+
+if [ -d "$smtpClient" ]; then
+	cd $smtpClient
+	git pull 
+else 
+	git clone https://github.com/bluetiger9/SmtpClient-for-Qt.git $smtpClient
+fi
+
+echo $DIR
+
